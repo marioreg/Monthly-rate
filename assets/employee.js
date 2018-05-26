d397cb4764e4a4ee1b7ebafd672f190015c9b1d8
Honey B <34516348+elizabethjmz@users.noreply.github.com>
2018-05-26 11:06:35 -0500

    Adding firebase info

--- assets/employee.js
+++ assets/employee.js
@@ -1,3 +1,17 @@
+//Firebase
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD9-mz9CuePPIYeaEEAdvLjJDRPi2hcXpU",
+    authDomain: "firstproject-cbe35.firebaseapp.com",
+    databaseURL: "https://firstproject-cbe35.firebaseio.com",
+    projectId: "firstproject-cbe35",
+    storageBucket: "firstproject-cbe35.appspot.com",
+    messagingSenderId: "43107698058"
+  };
+  firebase.initializeApp(config);
+
+  
+
 $("#submit").on("click", function(){
   
 
